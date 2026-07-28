38071e08e70d7266e7b99538d7e7458cef61d823
Sai Srinidhi Repala <srinidhirepala.com>
2026-07-28 23:17:55 +0530

    Feat:Added the Random User API feature

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,30 +1,50 @@
-/*alert which is used to display a message to the user when the page is loaded*/
-//alert("Hello, welcome to the Spring Boot Web Application!");
+// alert("Hello from javaScript");
 
 var users = [
     {
-        "image": "/images/jane.png",
-        "name": "Jane Doe",
-        "profession": "Software Engineer"
+        "image" : "/images/john.png",
+        "name" : "John Doe",
+        "gender" : "Male"
     },
     {
-        "image": "/images/john.png",
-        "name": "John Doe",
-        "profession": "Data Engineer"
+        "image" : "/images/jane.png",
+        "name" : "Jane Doe",
+        "gender" : "Female"
     }
-
-];
+]
 
 var id = 0;
 
-function toggleUser() {
+function toggleUser(){
     id = (id + 1) % users.length;
-
     var userImage = document.getElementById("user-image");
     var userName = document.getElementById("user-name");
-    var userProfession = document.getElementById("user-profession ");
+    var userGender = document.getElementById("user-gender");
 
     userImage.src = users[id].image;
-    userName.textContent = users[id].name;
-    userProfession.textContent = users[id].gender;
+    userName.innerHTML = users[id].name;
+    userGender.innerHTML = users[id].gender;
+}
+
+function randomUser(){
+    console.log("function called");
+    fetch("https://randomuser.me/api")
+        .then(function(res){
+            return res.json();
+        })
+        .then(function(data){
+            var userImage = document.getElementById("user-image");
+            var userName = document.getElementById("user-name");
+            var userGender = document.getElementById("user-gender");
+
+            userImage.src = data.results[0].picture.large;
+            userName.innerHTML = data.results[0].name.first + " " + data.results[0].name.second;
+            userGender.innerHTML = data.results[0].gender;
+
+        })
+        .error(function(err){
+            console.log(err);
+        })
+
+
 }
